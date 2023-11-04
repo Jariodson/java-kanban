e99012afc81ff508d4af20aeac3ae32ca26a462d
Jarodson <jarodson@yandex.ru>
2023-11-04 19:08:28 +0300

    fix: добавить в метод add проверку параметра на null, сделать размер листа константой, изменить возврат списка(вмместо ссылки вернуть копию)

--- src/Managers/HistoryManager/InMemoryHistoryManager.java
+++ src/Managers/HistoryManager/InMemoryHistoryManager.java
@@ -2,22 +2,29 @@ package Managers.HistoryManager;
 
 import Tasks.Task;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final List<Task> lastTenTasks = new ArrayList<>();
+    private static final int COUNT_OF_TASKS = 10;
+    private static final int FIRST_TASK = 0;
 
     @Override
     public void add(Task task) {
+        if (task == null){
+            return;
+        }
         lastTenTasks.add(task);
-        if (lastTenTasks.size() > 10) {
-            lastTenTasks.remove(0);
+        if (lastTenTasks.size() > COUNT_OF_TASKS) {
+            lastTenTasks.remove(FIRST_TASK);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return lastTenTasks;
+        return new ArrayList<>(lastTenTasks);
     }
 }
